1fc5c94f2341f0ff6ef5f7f51e6cc8cd500b8041
t-osawa-009 <da87435@gmail.com>
2019-12-06 21:24:06 +0900

    Initial Commit

---
+++ SpreadsheetClient/SpreadsheetClient.h
@@ -0,0 +1,19 @@
+//
+//  SpreadsheetClient.h
+//  SpreadsheetClient
+//
+//  Created by Takuya Ohsawa on 2019/12/06.
+//  Copyright © 2019 Takuya Ohsawa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SpreadsheetClient.
+FOUNDATION_EXPORT double SpreadsheetClientVersionNumber;
+
+//! Project version string for SpreadsheetClient.
+FOUNDATION_EXPORT const unsigned char SpreadsheetClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SpreadsheetClient/PublicHeader.h>
+
+
