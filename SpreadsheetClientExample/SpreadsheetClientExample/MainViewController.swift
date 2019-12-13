476ec439fd474ade87105b95729e43f48597b25c
t-osawa-009 <da87435@gmail.com>
2019-12-13 22:03:14 +0900

    Add carthage

--- SpreadsheetClientExample/SpreadsheetClientExample/MainViewController.swift
+++ SpreadsheetClientExample/SpreadsheetClientExample/MainViewController.swift
@@ -1,9 +1,20 @@
 import UIKit
+import SpreadsheetClient
 
 final class MainViewController: UITableViewController {
+    private let client = SpreadsheetClient()
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: String(describing:  UITableViewCell.self))
+        
+        client.send(id: "1zVw1G2LvoJOnnaez3Tuf2Kxqt7S8-zATNazY14FgBwI", sheetNumber: 1) { (result) in
+            switch result {
+            case .success(let dic):
+                print(dic)
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
